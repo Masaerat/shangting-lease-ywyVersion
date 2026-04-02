26c02d286ccb3d246b04a7f9463cf81eba261cd3
Masaerat <2625588564@qq.com>
2026-04-02 20:04:39 +0800

    增加缓存的prefix和缓存过期的时间常量 以及缓存一个空值用于缓存穿透

--- common/src/main/java/com/atguigu/lease/common/constant/RedisConstant.java
+++ common/src/main/java/com/atguigu/lease/common/constant/RedisConstant.java
@@ -12,5 +12,16 @@ public class RedisConstant {
     public static final String APP_LOGIN_PREFIX = "app:login:";
     public static final Integer APP_LOGIN_CODE_RESEND_TIME_SEC = 60;
     public static final Integer APP_LOGIN_CODE_TTL_SEC = 60 * 10;
-    public static final String APP_ROOM_PREFIX = "app:room:";
+
+    //房间详情缓存
+    public static final String APP_ROOM_DETAIL_PREFIX = "app:room:detail:";
+    public static final Integer APP_ROOM_DETAIL_TTL_SEC = 60 * 30; // 30分钟
+
+    //公寓简要信息缓存
+    public static final String APP_APARTMENT_ITEM_PREFIX = "app:apartment:item:";
+    public static final Integer APP_APARTMENT_ITEM_TTL_SEC = 60 * 60; // 1小时
+
+    //空值缓存（防止穿透）
+    public static final String CACHE_NULL_VALUE = "NULL";
+    public static final Integer CACHE_NULL_TTL_SEC = 60;
 }
