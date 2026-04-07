5f406decc3694950d91d7826d2e911ef5c3738a7
Masaerat <2625588564@qq.com>
2026-04-07 16:51:26 +0800

    定义通知消息实体类 根据后台管理系统的具体的业务来定义

---
+++ common/src/main/java/com/atguigu/lease/message/appointment/AppointmentNotificationMessage.java
@@ -0,0 +1,70 @@
+package com.atguigu.lease.message.appointment;
+
+import lombok.Data;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AppointmentNotificationMessage {
+
+    /**
+     * 预约ID
+     */
+    private Long appointmentId;
+
+    /**
+     * 用户ID
+     */
+    private Long userId;
+
+    /**
+     * 用户姓名
+     */
+    private String name;
+
+    /**
+     * 用户手机号
+     */
+    private String phone;
+
+    /**
+     * 公寓ID
+     */
+    private Long apartmentId;
+
+    /**
+     * 预约时间
+     */
+    private Date appointmentTime;
+
+    /**
+     * 备注信息
+     */
+    private String additionalInfo;
+
+    /**
+     * 预约状态
+     */
+    private String appointmentStatus;
+
+    /**
+     * 通知类型
+     */
+    private String notificationType; // CREATE, CANCEL, UPDATE
+
+    /**
+     * 通知内容
+     */
+    private String messageContent;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+}
